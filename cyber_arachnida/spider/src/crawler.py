cf393f0772b799d9d1bf9bba7b3ebddf158c888c
allemago <gbk.maelle@outlook.fr>
2026-03-31 08:41:59 +0200

    fix verif print max depth si recursive

--- cyber_arachnida/spider/src/crawler.py
+++ cyber_arachnida/spider/src/crawler.py
@@ -141,8 +141,9 @@ def spider(url: str, state: SpiderState) -> None:
     state.base_netloc = urlparse(url).netloc
     robots = load_robots_txt(url)
     state.path = create_folder(state.path)
-    print(
-        f"{Color.BOLD}Maximum depth level : "
-        f"{state.max_depth}{Color.RESET}"
-    )
+    if state.is_recursive:
+        print(
+            f"{Color.BOLD}Maximum depth level : "
+            f"{state.max_depth}{Color.RESET}"
+        )
     crawl(url, state, robots, 0)
